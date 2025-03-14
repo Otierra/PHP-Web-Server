369c2d782c5cbc971cfd8623f1b2569ed779926c
Otierra <135191584+Otierra@users.noreply.github.com>
2025-03-14 00:03:38 -0600

    cambio a conexion a Azure

--- Conexion.php
+++ Conexion.php
@@ -1,13 +1,28 @@
 <?php
 
-    class Conexion{
+    class Conexion {
+        function conectar() {
+            $host = "php-web-app-server.mysql.database.azure.com";
+            $dbname = "agendadb";
+            $username = "vgfsiroowd";
+            $password = 'VCXW$Yk8cWkpRcW0'; 
+            $port = 3306;
+            $charset = "utf8";
+            $ssl_cert_path = __DIR__ . "/DigiCertGlobalRootCA.crt.pem";
 
-        function conectar(){
-            $dsn = "mysql:host=localhost; dbname=agendadb;charset=utf8";
-            $pdo = new PDO($dsn, "root","");
-            return $pdo;
+            $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=$charset";
+            
+            try {
+                $pdo = new PDO($dsn, $username, $password, [
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                    PDO::MYSQL_ATTR_SSL_CA => $ssl_cert_path
+                ]);
+                return $pdo;
+            } catch (PDOException $e) {
+                die("Error de conexión: " . $e->getMessage());
+            }
         }
-
     }
+
 
 ?>
