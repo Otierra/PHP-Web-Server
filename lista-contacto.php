756e3694fc1bf435cf243787176003d3a1837068
Otierra <135191584+Otierra@users.noreply.github.com>
2025-03-14 10:05:35 -0600

    change list

--- lista-contacto.php
+++ lista-contacto.php
@@ -1,60 +1,63 @@
-<html>
+<?php
+session_start();
 
-    <head></head>
+if (!isset($_SESSION["usuario"])) {
+    header("Location: login.php");
+    exit();
+}
 
-    <body>
-        <?php
-            include_once("Contacto.php");
-            $contacto = new Contacto();
-            $arr = $contacto->listar();
-        ?>
+$usuarioActivo = isset($_SESSION["usuario"]) ? $_SESSION["usuario"] : (isset($_COOKIE["usuario"]) ? $_COOKIE["usuario"] : "");
+$tipoUsuario = isset($_SESSION["tipoUser"]) ? $_SESSION["tipoUser"] : (isset($_COOKIE["tipoUser"]) ? $_COOKIE["tipoUser"] : "");
 
-        <table>
-            <tr>
-                <th>Id</th> <th>Nombre</th> <th>Correo</th> <th>Telefono</th> <th>Operaciones</th>
-            </tr>
+include_once("Contacto.php");
+$contacto = new Contacto();
+$arr = $contacto->listar();
+?>
 
-            <?php
-                if (!empty($arr)) { 
-                    foreach($arr as $c){
-            ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de Contactos</title>
+</head>
+<body>
+    <h2>Usuario Activo: <?php echo htmlspecialchars($usuarioActivo); ?></h2>
+    <table>
+        <tr>
+            <th>Id</th> <th>Nombre</th> <th>Correo</th> <th>Telefono</th> <th>Operaciones</th>
+        </tr>
 
-            <tr>
-                <td><?php echo $c->id;?></td>
-                <td><?php echo $c->nombres;?></td>
-                <td><?php echo $c->correo;?></td>
-                <td><?php echo $c->telefono;?></td>
-                <td>
-                    <form action="eliminar_contacto.php" method="post">
-                        <input type="hidden" value="<?php echo $c->id;?>" name="id">
-                        <input type="submit" value="eliminar">
-                    </form>
-
-                </td>
-
-                <td>
-                    <form action="alta.php" method="post">
-                        <input type="hidden" value="<?php echo $c->id;?>" name="id">
-                        <input type="hidden" value="<?php echo $c->nombres;?>" name="nombre">
-                        <input type="hidden" value="<?php echo $c->telefono;?>" name="telefono">
-                        <input type="hidden" value="<?php echo $c->correo;?>" name="correo">
-                        <input type="submit" value="modificar">
-                    </form>
-
-                </td>
-
-            </tr>
-            
-            <?php
-                    }
-                }
-            ?>
-        
-
-        </table>
-
-        <a href="alta.php">Agregar Contacto</a>
-
-    </body>
-
+        <?php if (!empty($arr)) { 
+            foreach ($arr as $c) { ?>
+                <tr>
+                    <td><?php echo $c->id; ?></td>
+                    <td><?php echo $c->nombres; ?></td>
+                    <td><?php echo $c->correo; ?></td>
+                    <td><?php echo $c->telefono; ?></td>
+                    
+                    <?php if ($tipoUsuario === "tipoUser1") { ?>
+                        <td>
+                            <form action="eliminar_contacto.php" method="post">
+                                <input type="hidden" value="<?php echo $c->id; ?>" name="id">
+                                <input type="submit" value="Eliminar">
+                            </form>
+                        </td>
+                        <td>
+                            <form action="alta.php" method="post">
+                                <input type="hidden" value="<?php echo $c->id; ?>" name="id">
+                                <input type="hidden" value="<?php echo $c->nombres; ?>" name="nombre">
+                                <input type="hidden" value="<?php echo $c->telefono; ?>" name="telefono">
+                                <input type="hidden" value="<?php echo $c->correo; ?>" name="correo">
+                                <input type="submit" value="Modificar">
+                            </form>
+                        </td>
+                    <?php } ?>
+                </tr>
+        <?php }
+        } ?>
+    </table>
+    
+    <a href="alta.php">Agregar Contacto</a>
+</body>
 </html>
