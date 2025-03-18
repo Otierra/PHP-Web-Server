4bc92715c135086a2c7ba77c5654cfa3b3b89a6d
Otierra <135191584+Otierra@users.noreply.github.com>
2025-03-18 07:31:23 -0600

    delete head

--- lista-contacto.php
+++ lista-contacto.php
@@ -16,12 +16,6 @@
 
 <html>
 
-<head>
-    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
-    <meta http-equiv="Pragma" content="no-cache">
-    <meta http-equiv="Expires" content="0">
-</head>
-
 <body>
 
     <h2>Usuario Activo: <?php echo htmlspecialchars($usuarioActivo); ?></h2>
